11619339b24bab8a3ee38d45518f599d1dddd6ef
yijaejun <yijaejun81@gmail.com>
2016-12-31 12:42:56 +0900

    set bower

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,13 @@ require('laravel-elixir-vue-2');
  */
 
 elixir(mix => {
-    mix.sass('app.scss')
-       .webpack('app.js');
+    mix.sass('app.scss').webpack('app.js');
+    mix.scripts([
+        'resources/assets/bower_components/jquery/dist/jquery.min.js'
+    ]);
+    mix.styles([
+
+    ]);
+    // mix.copy('./vendor/bower_components/fontawesome/fonts', 'public/fonts');
+
 });
